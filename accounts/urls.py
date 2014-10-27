b588bd9085e5a4b0daab44c78775f6a01cdd3ffd
Michael Williams <draringi@draringi.net>
2014-10-27 12:22:50 -0400

    Added Registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,14 @@
 from django.conf.urls import patterns, url
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 urlpatterns = patterns('',
     #url(r'^$', UnfilledRequestsList.as_view()),
     url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'accounts/login.html'}),
+    url(r'^register/$', CreateView.as_view(
+            template_name='accounts/registration.html',
+            form_class=UserCreationForm,
+            success_url='/'
+    ), name="Registration"),
+
 )
